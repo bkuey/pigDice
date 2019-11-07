b5deebfc4c4342226c65149d47b625d7777763df
Mai Kue Lor and Will Quanstrom <mai@>
2019-11-06 16:25:29 -0800

    fix bug with NaN output

---
+++ README.md
@@ -0,0 +1,15 @@
+Specifications for Pig Dice:
+
+— Make a UI with two users with identical options.
+
+— Include two buttons for each player. One for roll and one for hold.
+
+— Use jQuery to collect user inputs.
+
+— Build an object called Player.
+
+— Include properties for name and score and total games won.
+
+— Write method prototypes that adds to score. One that adds to total games won. One that congratulates the winner. One that prints user’s name on their scorecard.
+
+— Invoke methods at appropriate times.
--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,6 @@ Player.prototype.printScore = function() {
   return this.score;
 }
 
-// Player.prototype.addScore = function() {
-//   this.score += roll;
-//   return this.score;
-//
-// }
-
 function Die(number) {
   this.number = number
 }
@@ -22,56 +16,70 @@ var dice = new Die();
 
 Die.prototype.roll = function() {
   roll = Math.ceil(Math.random() * 6);
-  turn = 0;
-
   if(roll == 1){
     alert("You rolled a 1!");
     player = !player;
-    return 0;
+    turn = 0;
+    roll = 0;
+    return turn;
   } else {
     return roll;
   }
 }
 
-
-var roll;
+var turn = 0;
+var roll = 0;
 $(document).ready(function() {
 
-var inputtedName1 = prompt('Player 1, enter your name!');
-$('#playerOne').text(inputtedName1);
-var score = 0;
-var totalWins = 0;
-var playerOne = new Player(inputtedName1, score, totalWins);
+  var inputtedName1 = prompt('Player 1, enter your name!');
+  $('#playerOne').text(inputtedName1);
+  var score = 0;
+  var totalWins = 0;
+  var playerOne = new Player(inputtedName1, score, totalWins);
 
-var inputtedName2 = prompt('Player 2, enter your name!');
-$('#playerTwo').text(inputtedName2);
-var score = 0;
-var totalWins = 0;
-var playerTwo = new Player(inputtedName2, score, totalWins);
-
+  var inputtedName2 = prompt('Player 2, enter your name!');
+  $('#playerTwo').text(inputtedName2);
+  // var score = 0;
+  // var totalWins = 0;
+  var playerTwo = new Player(inputtedName2, score, totalWins);
+  var total;
 
   $('#rollOne').click(function(){
     if(player == true){
     roll = dice.roll();
     $('#dieOne').append(roll);
-    // $('#oneScore').text(playerOne.addScore());
+    turn += roll;
   }
   });
-
-  $('#rollTwo').click(function(){
-    if(player == false){
-    roll = dice.roll();
-    $('#dieTwo').append(roll);
-    // $('#twoScore').text(playerTwo.addScore());
-  }
-  });
-  $('#holdTwo').click(function(){
-    if(player == false){
-      turn + playerTwo.score;
-      $('#twoScore').text(playerTwo.printScore());
-
+    $('#holdOne').click(function(){
+      if(player == true) {
+      total = playerOne.score + turn;
+      $('#oneScore').text(total);
+      playerOne.score = total;
+      console.log("Player 1 Score: " + playerOne.score);
+      player = !player;
+      turn = 0;
     }
-  })
+    });
+    var turn = 0;
+    $('#rollTwo').click(function(){
+      if(player == false){
+      roll = dice.roll();
+      $('#dieTwo').append(roll);
+      turn += roll;
+    }
+    });
+      $('#holdTwo').click(function(){
+        if(player == false) {
+        var total = playerTwo.score + turn;
+        $('#twoScore').text(total);
+        playerTwo.score = total;
+        console.log("Player 2 Score: " + playerTwo.score);
+        player = !player;
+        turn = 0;
+      }
+      });
+
 
 
 });
