9828a2cdebcad8aceda17b0cb2abda53cdbf5e68
Mai Kue Lor and Will Quanstrom <will@>
2019-11-07 10:15:59 -0800

    produce pigdice game in a bug-free two player mode with an alert message if one of the players reaches 100 and wins the game

--- js/scripts.js
+++ js/scripts.js
@@ -15,32 +15,32 @@ function Die(number) {
 var dice = new Die();
 
 Die.prototype.roll = function() {
-  roll = Math.ceil(Math.random() * 6);
+  var roll = Math.ceil(Math.random() * 6);
   if(roll == 1){
     alert("You rolled a 1!");
-    player = !player;
-    turn = 0;
+    turnOne = 0;
+    turnTwo = 0;
     roll = 0;
-    return turn;
+    player = !player;
+    return 0;
   } else {
     return roll;
   }
 }
 
-var turn = 0;
-var roll = 0;
+var turnOne = 0;
+var turnTwo = 0;
 $(document).ready(function() {
-
   var inputtedName1 = prompt('Player 1, enter your name!');
   $('#playerOne').text(inputtedName1);
   var score = 0;
   var totalWins = 0;
   var playerOne = new Player(inputtedName1, score, totalWins);
 
+
   var inputtedName2 = prompt('Player 2, enter your name!');
   $('#playerTwo').text(inputtedName2);
-  // var score = 0;
-  // var totalWins = 0;
+
   var playerTwo = new Player(inputtedName2, score, totalWins);
   var total;
 
@@ -48,17 +48,19 @@ $(document).ready(function() {
     if(player == true){
     roll = dice.roll();
     $('#dieOne').append(roll);
-    turn += roll;
-  }
+    turnOne += roll;
+    }
   });
     $('#holdOne').click(function(){
       if(player == true) {
-      total = playerOne.score + turn;
+      total = playerOne.score + turnOne;
       $('#oneScore').text(total);
       playerOne.score = total;
-      console.log("Player 1 Score: " + playerOne.score);
       player = !player;
-      turn = 0;
+      turnOne = 0;
+      if(playerOne.score >= 100){
+        return alert("You've reached 100! You are the winner!");
+      }
     }
     });
     var turn = 0;
@@ -66,20 +68,19 @@ $(document).ready(function() {
       if(player == false){
       roll = dice.roll();
       $('#dieTwo').append(roll);
-      turn += roll;
-    }
+      turnTwo += roll;
+      }
     });
       $('#holdTwo').click(function(){
         if(player == false) {
-        var total = playerTwo.score + turn;
+        total = playerTwo.score + turnTwo;
         $('#twoScore').text(total);
         playerTwo.score = total;
-        console.log("Player 2 Score: " + playerTwo.score);
         player = !player;
-        turn = 0;
+        turnTwo = 0;
+        if(playerTwo.score >= 100){
+          return alert("You've reached 100! You are the winner!");
+        }
       }
       });
-
-
-
 });
